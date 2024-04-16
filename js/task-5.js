181b900c7d77f879d8649eea39daab1e3c71ffaa
kholshevnikova <151967304+kholshevnikova@users.noreply.github.com>
2024-04-16 16:16:52 +0300

    update task-5

--- js/task-5.js
+++ js/task-5.js
@@ -3,3 +3,17 @@ function getRandomHexColor() {
     .toString(16)
     .padStart(6, 0)}`;
 }
+
+const buttonChangeColor = document.querySelector(".change-color");
+const body = document.querySelector("body");
+const spanColor = document.querySelector(".color");
+// console.log(buttonChangeColor);
+// console.log(body);
+// console.log(spanColor);
+
+buttonChangeColor.addEventListener("click", onClickMe);
+function onClickMe() {
+  const randomColor = getRandomHexColor();
+  body.style.backgroundColor = randomColor;
+  spanColor.textContent = randomColor;
+}
