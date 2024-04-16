9ab4e8c1b417ea30faba2ca53087a1e1af90dd93
kholshevnikova <151967304+kholshevnikova@users.noreply.github.com>
2024-04-16 15:31:20 +0300

    update Task-4

--- js/task-4.js
+++ js/task-4.js
@@ -1,1 +1,21 @@
+const form = document.querySelector(".login-form");
 
+form.addEventListener("submit", handleSubmit);
+function handleSubmit(event) {
+  event.preventDefault();
+  const form = event.target;
+  const email = form.elements.email.value.trim();
+  const password = form.elements.password.value.trim();
+
+  if (email === "" || password === "") {
+    alert("All form fields must be filled in");
+  }
+
+  const info = {
+    email: email,
+    password: password,
+  };
+
+  console.log(info);
+  form.reset();
+}
